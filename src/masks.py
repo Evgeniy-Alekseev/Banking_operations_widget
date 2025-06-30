91b1c5a434e91cac2d32a834c2a8445536ffe09d
Evgeniy Alekseev <e7777777a@gmail.com>
2025-06-30 17:56:18 +0200

    add logger for masks.py improved logger for utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,23 +1,43 @@
+import logging
+
+logging.basicConfig(
+    filename='../logs/masks.log',
+    encoding="utf-8",
+    filemode='w',
+    format='%(asctime)s %(name)s %(levelname)s: %(message)s',
+    level=logging.DEBUG
+)
+
+card_num_logger = logging.getLogger('add.card_num')
+account_logger = logging.getLogger('add.account')
+
+
 def get_mask_card_number(card_number: str) -> str:
     """
     Функция принимает на вход номер карты в виде числа и возвращает маску номера по правилу XXXX XX** **** XXXX
     """
+    card_num_logger.info('Запуск приложения')
     cleaned_number = card_number.replace(" ", "")
 
     # Проверка на пустую строку или строку из пробелов
+    card_num_logger.info('Проверка введённых данных')
     if not cleaned_number:
         raise ValueError("Номер карты не может быть пустым")
-
+        card_num_logger.error('Ошибка ввода данных')
     # Проверка на наличие только цифр
     if not cleaned_number.isdigit():
         raise ValueError("Номер карты должен содержать только цифры")
+        card_num_logger.error('Ошибка ввода данных')
 
     # Проверка длины номера карты
     if len(cleaned_number) != 16:
         raise ValueError("Номер карты должен содержать 16 цифр")
+        card_num_logger.error('Ошибка ввода данных')
 
-    # Остальная логика маскирования
+    # Собираем замаскированный номер
+    card_num_logger.info('Номер замаскирован успешно')
     return f"{cleaned_number[:4]} {cleaned_number[4:6]}** **** {cleaned_number[-4:]}"
+    card_num_logger.info('Завершение работы приложения')
 
 
 if __name__ == "__main__":
@@ -25,25 +45,36 @@ if __name__ == "__main__":
 
 
 def get_mask_account(account_number: str) -> str:
+
     """Функция принимает на вход номер счета и возвращает маску номера по правилу **XXXX"""
+
+    account_logger.info('Запуск приложения')
+    account_logger.info('Проверка введённых данных')
     if account_number is None:
         raise ValueError("Номер счёта не может быть None")
+        account_logger.error('Ошибка ввода данных')
 
     if not isinstance(account_number, str):
         raise ValueError("Номер счёта должен быть строкой")
+        account_logger.error('Ошибка ввода данных')
 
     cleaned_number = account_number.replace(" ", "")
 
     if not cleaned_number:
         raise ValueError("Номер счёта не может быть пустым")
+        account_logger.error('Ошибка ввода данных')
 
     if not cleaned_number.isdigit():
         raise ValueError("Номер счёта должен содержать только цифры")
+        account_logger.error('Ошибка ввода данных')
 
     if len(cleaned_number) < 4:
         raise ValueError("Номер счёта должен содержать минимум 4 цифры")
+        account_logger.error('Ошибка ввода данных')
 
+    account_logger.info('Номер замаскирован успешно')
     return f"**{cleaned_number[-4:]}"
+    account_logger.info('Завершение работы приложения')
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,14 @@
 import json
 import logging
-import os
 from pathlib import Path
 from typing import Dict, List
-
 
 logging.basicConfig(
     filename='../logs/utils.log',
     encoding="utf-8",
     filemode='w',
     format='%(asctime)s %(name)s %(levelname)s: %(message)s',
-    level=logging.INFO
+    level=logging.DEBUG
 )
 
 logger = logging.getLogger(__name__)
@@ -26,20 +24,23 @@ def load_transactions(file_path: str = "operations.json") -> List[Dict]:
         if not Path(file_path).exists():
             return []
 
+        logger.info('Запуск чтения данных из файла')
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
-
+            logger.info('Данные загружены успешно')
             if isinstance(data, list):
                 return data
             return []
-
-    except (json.JSONDecodeError, FileNotFoundError, PermissionError):
+            logger.info('Данные для загрузки не найдены')
+    except (json.JSONDecodeError, FileNotFoundError, PermissionError) as ex:
         # Обрабатываем ошибки:
         # - Невалидный JSON
         # - Файл не найден
         # - Нет прав на чтение
+        logger.error(f'Ошибка {ex}')
         return []
-
+        logger.info('Завершение работы приложения')
 
 if __name__ == "__main__":
     load_transactions()
+
