4d22415f624d208cc59b0ddf4d710446d8ca005b
Evgeniy Alekseev <e7777777a@gmail.com>
2025-07-06 01:26:49 +0200

    after checking with linters

--- src/utils_2.py
+++ src/utils_2.py
@@ -1,7 +1,8 @@
 import csv
-import pandas as pd
 from pathlib import Path
 from typing import Dict, List
+
+import pandas as pd
 
 
 def load_transactions_csv(file_path: str = "transactions.csv") -> List[Dict]:
@@ -13,17 +14,16 @@ def load_transactions_csv(file_path: str = "transactions.csv") -> List[Dict]:
         if not Path(file_path).exists():
             return []
 
-        with open('transactions.csv', 'r', encoding="utf-8") as file:
+        with open("transactions.csv", "r", encoding="utf-8") as file:
             data_csv = list(csv.DictReader(file))
             return data_csv
-
 
     except (csv.Error, FileNotFoundError, PermissionError):
         # Обрабатываем ошибки:
         # - Невалидный CSV
         # - Файл не найден
         # - Нет прав на чтение
-       return []
+        return []
 
 
 if __name__ == "__main__":
@@ -39,19 +39,18 @@ def load_transactions_excel(file_path: str = "transactions_excel.xlsx") -> List[
         if not Path(file_path).exists():
             return []
 
-        df = pd.read_excel('transactions_excel.xlsx')
-        df_excel = df.to_dict('records')
+        df = pd.read_excel("transactions_excel.xlsx")
+        df_excel = df.to_dict("records")
         return df_excel
 
-    except (FileNotFoundError, PermissionError, pd.errors.EmptyDataError,
-            ValueError, KeyError, ImportError):
+    except (FileNotFoundError, PermissionError, pd.errors.EmptyDataError, ValueError, KeyError, ImportError):
         # Обрабатываем ошибки:
         # - Невалидный EXCEL
         # - Файл не найден
         # - Нет прав на чтение
         # - Неверная структура массива данных
         # - Неверный импорт
-       return []
+        return []
 
 
 if __name__ == "__main__":
